6be7884ef948b4c775b70fc84a94ed779caf4884
smark-mahi <sumeriamahi1906@gmail.com>
2023-01-23 06:21:34 +0530

    movies

--- src/features/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "./products/products";
-
-export const store = configureStore({
-  reducer: {
-    movies: productsReducer
-  },
-});
